b2c113b9eea2dcdd74ddeb7d5322646db418f6b0
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-03 13:04:33 +0300

    styled the scrollBarMenu but it's vertical i will try to modify it later on

--- gym/src/pages/Home.jsx
+++ gym/src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import React ,{ useState }from 'react'
-import { Box } from '@mui/material';
-import Exercises from '../components/Exercises';
-import SearchExercises from '../components/SearchExercises';
-import HeroBanner from '../components/HeroBanner';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercise, setExercise] = useState([]);
   return (
     <Box>
-      <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercise}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises 
+        setExercises={setExercise}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
